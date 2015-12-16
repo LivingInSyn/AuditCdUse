aedf17679f296ae28b5ab4ff04e5b69192be25fe
Jeremy Mill <jeremymill@gmail.com>
2015-12-15 21:41:01 -0500

    SOOOO much cleaner
    might be working? Need to check at the office tomorrow

--- AuditCdUse/AuditCdUse/AuditMain.cs
+++ AuditCdUse/AuditCdUse/AuditMain.cs
@@ -15,47 +15,16 @@ namespace AuditCdUse
 {
     class AuditMain
     {
-        MySqlConn con;
-        ManualResetEvent mre;
         static bool should_exit = false;
-        static StreamWriter file;
 
         static void Main(string[] args)
         {
             //add an event handler for when a user shutdown down or logs off
             SystemEvents.SessionEnding += Detect_Logout;
+            MySqlConn connection = new MySqlConn();
 
             AuditMain am = new AuditMain();
-            /*ManagementEventWatcher w = null;
-            WqlEventQuery q;
-            ManagementOperationObserver observer = new ManagementOperationObserver();
-
-            // Bind to local machine
-            ConnectionOptions opt = new ConnectionOptions();
-            opt.EnablePrivileges = true; //sets required privilege
-            ManagementScope scope = new ManagementScope("root\\CIMV2", opt);
-
-            q = new WqlEventQuery();
-            q.EventClassName = "__InstanceModificationEvent";
-            q.WithinInterval = new TimeSpan(0, 0, 1);
-
-            // DriveType - 5: CDROM
-            q.Condition = @"TargetInstance ISA 'Win32_LogicalDisk' and
-            TargetInstance.DriveType = 5";
-            w = new ManagementEventWatcher(scope, q);
-
-            // register async. event handler
-            w.EventArrived += new EventArrivedEventHandler(am.CDREventArrived);
-            //it was hanging up with start outside this loop and stop after it, I'll do more experimenting
-            //but currently, this works, and doesn't do anything crazy resource wise
-
-            file.WriteLine(DateTime.Now.ToString() + " Before the while");
-            while (should_exit != 1)
-            {
-                w.Start();
-                w.WaitForNextEvent();
-                w.Stop();
-            }*/
+            
             string lastTitle = "";
             while (should_exit == false)
             {
@@ -66,7 +35,9 @@ namespace AuditCdUse
                         if(drive.VolumeLabel != lastTitle)
                         {
                             lastTitle = drive.VolumeLabel;
-                            //need to raise an event here and make ReportCd consume it
+                            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+                            string machineName = Environment.MachineName;
+                            connection.InsertCdEvent(machineName, userName);
                         }
                     }
                     else
@@ -79,58 +50,7 @@ namespace AuditCdUse
             
             return;
         }
-
-        public void CDREventArrived(object sender, EventArrivedEventArgs e)
-        {
-            // Get the Event object and display it
-            PropertyData pd = e.NewEvent.Properties["TargetInstance"];
-
-            if (pd != null)
-            {
-                ManagementBaseObject mbo = pd.Value as ManagementBaseObject;
-
-                // if CD removed VolumeName == null
-                if (mbo.Properties["VolumeName"].Value != null)
-                {
-                    file.WriteLine(DateTime.Now.ToString() + " CD Insert Detected");
-                    //Console.WriteLine("CD has been inserted");
-                    string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-                    string machineName = Environment.MachineName;
-                    int sent = con.InsertCdEvent(machineName, userName);
-                    if(sent == 1)
-                    {
-                        //sent
-                        file.WriteLine(DateTime.Now.ToString() + " event sent");
-                    }
-                    else
-                    {
-                        //not sent
-                        file.WriteLine(DateTime.Now.ToString() + " event NOT sent");
-                    }
-                }
-                else
-                {
-                    //Debug.WriteLine("CD has been ejected");
-                }
-            }
-        }
-
-        public void ReportCd()
-        {
-            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
-            string machineName = Environment.MachineName;
-            int sent = con.InsertCdEvent(machineName, userName);
-        }
-
-        public AuditMain()
-        {
-            con = new MySqlConn();
-            mre = new ManualResetEvent(false);
-            file = new StreamWriter(@"C:\uits\auditCdLog.txt");
-            file.AutoFlush = true;
-            file.WriteLine(DateTime.Now.ToString() + " AuditMain started");
-        }
-
+        
         private static void Detect_Logout(object sender, EventArgs e)
         {
             should_exit = true;
